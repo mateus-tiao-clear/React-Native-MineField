e90dfed03b56ddcc7adf697aed7651a2047aa880
mateus gomes <mateusgomesc04@gmail.com>
2023-07-07 07:44:40 -0300

    feat: creating Mine components

---
+++ src/components/Mine/index.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './styles';
+
+const Mine = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.coreMine} />
+      <View style={styles.line} />
+      <View style={[styles.line, { transform: [{ rotate: '45deg' }] }]} />
+      <View style={[styles.line, { transform: [{ rotate: '90deg' }] }]} />
+      <View style={[styles.line, { transform: [{ rotate: '135deg' }] }]} />
+    </View>
+  )
+}
+
+export { Mine };
